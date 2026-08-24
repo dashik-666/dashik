0027a93f1e2c6abde17a539a735a54ef8d76a577
dashik-666 <2902820075@qq.com>
2026-08-24 10:21:31 +0800

    feat: 生产部署适配

--- storefront/src/router/index.ts
+++ storefront/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { TOKEN_KEY } from '@/api';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: () => import('@/views/Home.vue'), meta: { title: '首页' } },
     { path: '/workers', name: 'workers', component: () => import('@/views/Workers.vue'), meta: { title: '陪玩广场' } },
